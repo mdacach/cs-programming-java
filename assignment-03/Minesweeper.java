166758113a164273d2224caf94ea3703393aac92
matheus <dacachmatheus@gmail.com>
2020-03-04 21:34:56 -0300

    add corrections for assignment 03

--- assignment-03/Minesweeper.java
+++ assignment-03/Minesweeper.java
@@ -4,44 +4,26 @@ public class Minesweeper {
         int n = Integer.parseInt(args[1]);
         int k = Integer.parseInt(args[2]);
         String[][] grid = new String[m][n];
-        int[][] count = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = "O";
+            }
+        }
+
+        int[][] count = new int[m + 2][n + 2];
         int countMines = 0;
 
         while (countMines < k) {
 
             int x = (int) (Math.random() * m);
             int y = (int) (Math.random() * n);
-
-            if (grid[x][y].equals("*"))
-                continue;
-            else {
+            if (!grid[x][y].equals("*")) {
                 grid[x][y] = "*";
-                boolean left = (x - 1 >= 0);
-                boolean right = (x + 1 < m);
-                boolean up = (y - 1 >= 0);
-                boolean down = (y + 1 > n);
-                boolean leftUpDiag = (left && up);
-                boolean leftDownDiag = (left && down);
-                boolean rightUpDiag = (right && up);
-                boolean rightDownDiag = (right && down);
-
-                if (left)
-                    count[x - 1][y]++;
-                if (right)
-                    count[x + 1][y]++;
-                if (up)
-                    count[x][y - 1]++;
-                if (down)
-                    count[x][y - 1]++;
-                if (leftUpDiag)
-                    count[x - 1][y - 1]++;
-                if (leftDownDiag)
-                    count[x - 1][y + 1]++;
-                if (rightUpDiag)
-                    count[x + 1][y - 1]++;
-                if (rightDownDiag)
-                    count[x + 1][y + 1]++;
-
+                for (int i = x; i <= x + 2; i++) {
+                    for (int j = y; j <= y + 2; j++) {
+                        count[i][j]++;
+                    }
+                }
                 countMines++;
             }
         }
@@ -49,7 +31,7 @@ public class Minesweeper {
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (!grid[i][j].equals("*"))
-                    System.out.print(count[i][j] + "  ");
+                    System.out.print(count[i + 1][j + 1] + "  ");
                 else
                     System.out.print(grid[i][j] + "  ");
             }
