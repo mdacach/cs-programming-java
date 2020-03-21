6c906dd95cc4565480e9daf5bc93596d5522b96f
matheus <dacachmatheus@gmail.com>
2020-03-21 20:52:03 -0300

    add assignment 10

---
+++ assignment-10/BarChartRacer.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class BarChartRacer {
+
+    public static void main(String[] args) {
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setCanvasSize(800, 800);
+        String filename = args[0];
+        int k = Integer.parseInt(args[1]);
+        In reader = new In(filename);
+
+        String title, xAxisLabel, dataSource;
+        title = reader.readLine();
+        xAxisLabel = reader.readLine();
+        dataSource = reader.readLine();
+        BarChart barChart = new BarChart(title, xAxisLabel, dataSource);
+        while (reader.hasNextLine()) {
+            reader.readLine(); // blank line
+
+            int n = Integer.parseInt(reader.readLine());
+            Bar[] bars = new Bar[n];
+            for (int i = 0; i < n; i++) {
+                String line = reader.readLine();
+                String[] arguments = line.split(",");
+                bars[i] = new Bar(arguments[1], Integer.parseInt(arguments[3]), arguments[4]);
+                barChart.setCaption(arguments[0]);
+            }
+
+            Arrays.sort(bars);
+            for (int i = n - 1; i >= Math.max(n - k, 0); i--) {
+                barChart.add(bars[i].getName(), bars[i].getValue(), bars[i].getCategory());
+            }
+
+            StdDraw.clear();
+            barChart.draw();
+            barChart.reset();
+            StdDraw.show();
+            StdDraw.pause(3);
+
+        }
+    }
+}
